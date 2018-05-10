deb4edfced3b14cb9f33253f5833858db055c70f
Tina Albores <maalbores@gmail.com>
2018-05-10 17:26:30 +0800

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_110254) do
+ActiveRecord::Schema.define(version: 2018_05_10_073043) do
 
   create_table "my_classes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_my_classes_on_name", unique: true
+  end
+
+  create_table "my_classes_professors", id: false, force: :cascade do |t|
+    t.integer "my_class_id", null: false
+    t.integer "professor_id", null: false
+    t.index ["my_class_id", "professor_id"], name: "index_my_classes_professors_on_my_class_id_and_professor_id"
+    t.index ["professor_id", "my_class_id"], name: "index_my_classes_professors_on_professor_id_and_my_class_id"
+  end
+
+  create_table "professors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["first_name"], name: "index_professors_on_first_name"
+    t.index ["last_name"], name: "index_professors_on_last_name"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "professor_id"
+    t.integer "my_class_id"
+    t.index ["my_class_id"], name: "index_reviews_on_my_class_id"
+    t.index ["professor_id"], name: "index_reviews_on_professor_id"
   end
 
 end
