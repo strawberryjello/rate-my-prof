bd8994ab42c870a3b682bc1803819934ebe37917
Tina Albores <maalbores@gmail.com>
2018-05-10 16:57:49 +0800

    nested Review resource under Professor in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :reviews
+
+  resources :professors do
+    resources :reviews
+  end
+
   resources :my_classes
-  resources :professors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
