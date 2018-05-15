d8dbc1194759ddf6bd763d8bb7953d8a1147ab3c
Tina Albores <maalbores@gmail.com>
2018-05-15 17:07:12 +0800

    added tests for Professor#average_rating

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -53,4 +53,51 @@ RSpec.describe Professor do
       end
     end
   end
+
+  describe '#average_rating' do
+    fixtures :my_classes, :professors, :reviews
+
+    context 'with no classes associated' do
+      let(:prof) { professors(:no_class) }
+
+      it 'returns nil' do
+        expect(prof.average_rating).to be_nil
+      end
+    end
+
+    context 'with 1 unreviewed class' do
+      let(:prof) { professors(:one_unreviewed_class) }
+
+      it 'returns nil' do
+        expect(prof.average_rating).to be_nil
+      end
+    end
+
+    context 'with 1 reviewed class that has 1 review' do
+      let(:prof) { professors(:one_reviewed_class) }
+
+      it 'returns the rating for that review' do
+        my_class = prof.my_classes.first
+        review = my_class.reviews.first
+
+        expect(prof.average_rating).to eq(review.rating)
+      end
+    end
+
+    context 'with 1 reviewed class that has multiple reviews' do
+      let(:prof) { professors(:one_class_multiple_reviews) }
+
+      it 'returns the average rating for the reviews' do
+        expect(prof.average_rating).to eq(2.33)
+      end
+    end
+
+    context 'with multiple reviewed classes with multiple reviews' do
+      let(:prof) { professors(:multiple_classes_multiple_reviews) }
+
+      it 'returns the average rating for the reviews' do
+        expect(prof.average_rating).to eq(4)
+      end
+    end
+  end
 end
